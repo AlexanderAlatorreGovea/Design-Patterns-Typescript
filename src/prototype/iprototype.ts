3451f8e51cd308c264a07a178d03dd6eeb375b6c
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-10 19:37:03 -0800

    added linting

--- src/prototype/iprototype.ts
+++ src/prototype/iprototype.ts
@@ -3,9 +3,9 @@
 import Document from "./document";
 
 export default interface IProtoType {
-    // interface with clone method
-    clone(mode: number): Document
-    // The clone, deep or shallow.
-    // It is up to you how you  want to implement
-    // the details in your concrete class"""
+  // interface with clone method
+  clone(mode: number): Document;
+  // The clone, deep or shallow.
+  // It is up to you how you  want to implement
+  // the details in your concrete class"""
 }
