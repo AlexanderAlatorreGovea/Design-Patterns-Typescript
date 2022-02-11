dae843b4d13536edd9613d3ef720b6c9efab68f3
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-10 18:06:38 -0800

    added linting

--- src/builder/houseboat-director.ts
+++ src/builder/houseboat-director.ts
@@ -1,14 +1,14 @@
 // A Director Class
-import House from './house'
-import HouseBuilder from './house-builder'
+import House from "./house";
+import HouseBuilder from "./house-builder";
 
 export default class HouseBoatDirector {
-    static construct(): House {
-        return new HouseBuilder()
-            .setBuildingType('House Boat')
-            .setWallMaterial('Wood')
-            .setNumberDoors(6)
-            .setNumberWindows(8)
-            .getResult()
-    }
+  static construct(): House {
+    return new HouseBuilder()
+      .setBuildingType("House Boat")
+      .setWallMaterial("Wood")
+      .setNumberDoors(6)
+      .setNumberWindows(8)
+      .getResult();
+  }
 }
--- src/builder/igloo-director.ts
+++ src/builder/igloo-director.ts
@@ -1,16 +1,16 @@
 // A Director Class
-import House from './house'
-import HouseBuilder from './house-builder'
+import House from "./house";
+import HouseBuilder from "./house-builder";
 
 export default class IglooDirector {
-    static construct(): House {
-        // Note that in this IglooDirector, it has omitted the
-        // set_number_of windows call since this Igloo will have
-        // no windows.
-        return new HouseBuilder()
-            .setBuildingType('Igloo')
-            .setWallMaterial('Ice')
-            .setNumberDoors(1)
-            .getResult()
-    }
+  static construct(): House {
+    // Note that in this IglooDirector, it has omitted the
+    // set_number_of windows call since this Igloo will have
+    // no windows.
+    return new HouseBuilder()
+      .setBuildingType("Igloo")
+      .setWallMaterial("Ice")
+      .setNumberDoors(1)
+      .getResult();
+  }
 }
