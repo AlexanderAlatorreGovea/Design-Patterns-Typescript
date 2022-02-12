561394de467513cbedf99a155ffb5ce42da2586d
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-12 15:56:47 -0800

    fixed linting

--- src/composite/file.ts
+++ src/composite/file.ts
@@ -1,24 +1,24 @@
-import IComponent from './icomponent'
-import Folder from './folder'
+import IComponent from "./icomponent";
+import Folder from "./folder";
 
 export default class File implements IComponent {
-    // The File Class. The files are the leaves
+  // The File Class. The files are the leaves
 
-    name: string
-    referenceToParent?: Folder = undefined
+  name: string;
+  referenceToParent?: Folder = undefined;
 
-    constructor(name: string) {
-        this.name = name
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  dir(indent: string): void {
+    console.log(`${indent}<FILE> ${this.name}`);
+  }
+
+  detach(): void {
+    "Detaching this leaf from its parent composite";
+    if (this.referenceToParent) {
+      this.referenceToParent.delete(this);
     }
-
-    dir(indent: string): void {
-        console.log(`${indent}<FILE> ${this.name}`)
-    }
-
-    detach(): void {
-        'Detaching this leaf from its parent composite'
-        if (this.referenceToParent) {
-            this.referenceToParent.delete(this)
-        }
-    }
+  }
 }
--- src/composite/icomponent.ts
+++ src/composite/icomponent.ts
@@ -1,14 +1,14 @@
-import Folder from './folder'
+import Folder from "./folder";
 
 export default interface IComponent {
-    // A component interface describing the common
-    // fields and methods of leaves and composites
+  // A component interface describing the common
+  // fields and methods of leaves and composites
 
-    referenceToParent?: Folder
+  referenceToParent?: Folder;
 
-    dir(indent: string): void
-    // A method each Leaf and composite container should implement
+  dir(indent: string): void;
+  // A method each Leaf and composite container should implement
 
-    detach(): void
-    // Called before a leaf is attached to a composite
+  detach(): void;
+  // Called before a leaf is attached to a composite
 }
