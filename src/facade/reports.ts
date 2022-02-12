5526322e407ccb923942f512495900667cf1fab7
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-12 14:04:43 -0800

    fixed linting

--- src/facade/reports.ts
+++ src/facade/reports.ts
@@ -1,24 +1,24 @@
 // A Singleton Dictionary of Reported Events
 
 export default class Reports {
-    static instance: Reports
-    #reports: { [id: string]: [number, string] } = {}
-    #rowId = 0
+  static instance: Reports;
+  #reports: { [id: string]: [number, string] } = {};
+  #rowId = 0;
 
-    constructor() {
-        if (Reports.instance) {
-            return Reports.instance
-        }
-        Reports.instance = this
+  constructor() {
+    if (Reports.instance) {
+      return Reports.instance;
     }
+    Reports.instance = this;
+  }
 
-    getHistory(): { [id: string]: [number, string] } {
-        return this.#reports
-    }
+  getHistory(): { [id: string]: [number, string] } {
+    return this.#reports;
+  }
 
-    logEvent(event: string): boolean {
-        this.#reports[this.#rowId] = [Date.now(), event]
-        this.#rowId = this.#rowId + 1
-        return true
-    }
+  logEvent(event: string): boolean {
+    this.#reports[this.#rowId] = [Date.now(), event];
+    this.#rowId = this.#rowId + 1;
+    return true;
+  }
 }
