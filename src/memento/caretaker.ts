f9440579cee428750fec58e18ea7e052cb4e75c9
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-13 19:15:46 -0700

    fixed linting

--- src/memento/caretaker.ts
+++ src/memento/caretaker.ts
@@ -1,31 +1,31 @@
 // The Save/Restore Game functionality
 
-import GameCharacter from './game-character'
-import Memento from './memento'
+import GameCharacter from "./game-character";
+import Memento from "./memento";
 
 export default class CareTaker {
-    // Guardian. Provides a narrow interface to the mementos
+  // Guardian. Provides a narrow interface to the mementos
 
-    #originator: GameCharacter
-    #mementos: Memento[]
+  #originator: GameCharacter;
+  #mementos: Memento[];
 
-    constructor(originator: GameCharacter) {
-        this.#originator = originator
-        this.#mementos = []
-    }
+  constructor(originator: GameCharacter) {
+    this.#originator = originator;
+    this.#mementos = [];
+  }
 
-    save(): void {
-        // Store a new Memento of the Characters current state
-        console.log('CareTaker: Game Save')
-        const memento = this.#originator.memento
-        this.#mementos.push(memento)
-    }
+  save(): void {
+    // Store a new Memento of the Characters current state
+    console.log("CareTaker: Game Save");
+    const memento = this.#originator.memento;
+    this.#mementos.push(memento);
+  }
 
-    restore(index: number): void {
-        // Replace the Characters current attributes with the state
-        // stored in the saved Memento
-        console.log('CareTaker: Restoring Characters attributes from Memento')
-        const memento = this.#mementos[index]
-        this.#originator.memento = memento
-    }
+  restore(index: number): void {
+    // Replace the Characters current attributes with the state
+    // stored in the saved Memento
+    console.log("CareTaker: Restoring Characters attributes from Memento");
+    const memento = this.#mementos[index];
+    this.#originator.memento = memento;
+  }
 }
