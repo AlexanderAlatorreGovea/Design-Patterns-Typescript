786c7107345af3affc8d5af65ca1e5c31685f1d3
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-13 21:15:14 -0700

    fixed linting

--- src/state/client.ts
+++ src/state/client.ts
@@ -1,83 +1,83 @@
 // The State Use Case Example
 
 enum ExampleState {
-    Initializing = 'Initializing',
-    Started = 'Started',
-    Running = 'Running',
-    Finished = 'Finished',
+  Initializing = "Initializing",
+  Started = "Started",
+  Running = "Running",
+  Finished = "Finished",
 }
 
 interface IExampleState {
-    // A State Interface
-    request(): void
+  // A State Interface
+  request(): void;
 }
 
 class StateContext implements IExampleState {
-    #state: ExampleState
+  #state: ExampleState;
 
-    constructor() {
-        this.#state = ExampleState.Initializing
-    }
+  constructor() {
+    this.#state = ExampleState.Initializing;
+  }
 
-    public get state() {
-        return this.#state
-    }
+  public get state() {
+    return this.#state;
+  }
 
-    public set state(value: ExampleState) {
-        switch (value) {
-            case ExampleState.Started:
-                this.request = Started.prototype.request
-                break
-            case ExampleState.Running:
-                this.request = Running.prototype.request
-                break
-            case ExampleState.Finished:
-                this.request = Finished.prototype.request
-                break
-        }
-        this.#state = value
+  public set state(value: ExampleState) {
+    switch (value) {
+      case ExampleState.Started:
+        this.request = Started.prototype.request;
+        break;
+      case ExampleState.Running:
+        this.request = Running.prototype.request;
+        break;
+      case ExampleState.Finished:
+        this.request = Finished.prototype.request;
+        break;
     }
+    this.#state = value;
+  }
 
-    request() {
-        // Does nothing until state changes, when then
-        // this method handle is reassigned to a different
-        // concrete states request method
-    }
+  request() {
+    // Does nothing until state changes, when then
+    // this method handle is reassigned to a different
+    // concrete states request method
+  }
 }
 
 class Started implements IExampleState {
-    // A ConcreteState Subclass
-    request() {
-        console.log(`I am now Started`)
-    }
+  // A ConcreteState Subclass
+  request() {
+    console.log(`I am now Started`);
+  }
 }
 
 class Running implements IExampleState {
-    // A ConcreteState Subclass
-    request() {
-        console.log(`I am now Running`)
-    }
+  // A ConcreteState Subclass
+  request() {
+    console.log(`I am now Running`);
+  }
 }
 
 class Finished implements IExampleState {
-    // A ConcreteState Subclass
-    request() {
-        console.log(`I am now Finished`)
-    }
+  // A ConcreteState Subclass
+  request() {
+    console.log(`I am now Finished`);
+  }
 }
 
 // The Client
-const STATE_CONTEXT = new StateContext()
-console.log('STATE_CONTEXT = ' + STATE_CONTEXT.state)
-STATE_CONTEXT.state = ExampleState.Started
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Running
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Finished
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Started
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Running
-STATE_CONTEXT.request()
-STATE_CONTEXT.state = ExampleState.Finished
-STATE_CONTEXT.request()
+const STATE_CONTEXT = new StateContext();
+console.log("STATE_CONTEXT = " + STATE_CONTEXT.state);
+STATE_CONTEXT.state = ExampleState.Started;
+STATE_CONTEXT.request();
+STATE_CONTEXT.state = ExampleState.Running;
+STATE_CONTEXT.request();
+STATE_CONTEXT.state = ExampleState.Finished;
+STATE_CONTEXT.request();
+STATE_CONTEXT.state = ExampleState.Started;
+STATE_CONTEXT.request();
+STATE_CONTEXT.state = ExampleState.Running;
+STATE_CONTEXT.request();
+STATE_CONTEXT.state = ExampleState.Finished;
+STATE_CONTEXT.request();
