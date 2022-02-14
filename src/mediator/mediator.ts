7ff2981c81c2aa1261d555c23a828b7f30aa6363
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-13 18:33:06 -0700

    fixed linting

--- src/mediator/mediator.ts
+++ src/mediator/mediator.ts
@@ -1,26 +1,26 @@
 // The Subject that all components will stay synchronized with
 
-import IComponent from './icomponent'
+import IComponent from "./icomponent";
 
 export default class Mediator {
-    // A Subject whose notify method is mediated
-    #components: Set<IComponent>
+  // A Subject whose notify method is mediated
+  #components: Set<IComponent>;
 
-    constructor() {
-        this.#components = new Set()
-    }
+  constructor() {
+    this.#components = new Set();
+  }
 
-    add(component: IComponent): void {
-        // Add components
-        this.#components.add(component)
-    }
+  add(component: IComponent): void {
+    // Add components
+    this.#components.add(component);
+  }
 
-    notify(message: string, originator: IComponent): void {
-        // Add components except for the originator component
-        this.#components.forEach((component) => {
-            if (component !== originator) {
-                component.receive(message)
-            }
-        })
-    }
+  notify(message: string, originator: IComponent): void {
+    // Add components except for the originator component
+    this.#components.forEach((component) => {
+      if (component !== originator) {
+        component.receive(message);
+      }
+    });
+  }
 }
